8abaa52db3929adf6461ef5b10f4f2272f70b5d0
siddharthbankar12 <siddharthbankar1204@gmail.com>
2025-05-23 22:51:28 +0530

    upated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://invoicemanagementsystem.vercel.app/",
     credentials: true,
   })
 );
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //   baseURL: "https://cromaclone.onrender.com/api/v1",
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://invoicemanagementsystem-536p.onrender.com/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
   withCredentials: true,
 });
 
